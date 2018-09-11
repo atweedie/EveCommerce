119e47fa22399c702f0035982adaa86a151446a7
Adam <adam_tweedie@msn.com>
2018-09-11 21:06:28 +0100

    Added some basic CSS

--- src/server/server.js
+++ src/server/server.js
@@ -4,12 +4,14 @@ import cookieParsrer from 'cookie-parser';
 import router from './routes/eveCommerce';
 
 const viewsDirectory = path.join(__dirname, '../app/templates');
+const staticsDirectory = path.join(__dirname, '../app/public');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', viewsDirectory)
 
+app.use(express.static(staticsDirectory));
 app.use(cookieParsrer())
 app.use('/', router);
 
